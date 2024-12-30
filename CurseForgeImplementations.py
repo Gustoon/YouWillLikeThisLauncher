dd63f7eb3a6faff6abbff3bcd1b6f87b9f342f4c
Gustoon <135733758+Gustoon@users.noreply.github.com>
2024-12-30 12:37:54 +0100

    added MidnightLib and some tweaks

--- CurseForgeImplementations.py
+++ CurseForgeImplementations.py
@@ -27,16 +27,18 @@ class CFimpl :
 
         self.Controlify = f"https://www.curseforge.com/minecraft/mc-mods/controlify/files/all?version={self.Version}&gameVersionTypeId={str(self.LoaderNmbr)}"
         self.MidnightControls = f"https://www.curseforge.com/minecraft/mc-mods/midnightcontrols/files/all?version={self.Version}&gameVersionTypeId={str(self.LoaderNmbr)}"
+        self.MidnightLib = f"https://www.curseforge.com/minecraft/mc-mods/midnightlib/files/all?version={self.Version}&gameVersionTypeId={str(self.LoaderNmbr)}"
 
         self.Controllable_Fo = f"https://www.curseforge.com/minecraft/mc-mods/controllable/files/all?version={self.Version}&gameVersionTypeId={str(self.LoaderNmbr)}"
         self.Controllable_Fa = f"https://www.curseforge.com/minecraft/mc-mods/controllable-fabric/files/all?version={self.Version}&gameVersionTypeId={str(self.LoaderNmbr)}"
         self.Framework = f"https://www.curseforge.com/minecraft/mc-mods/framework/files/all?version={self.Version}&gameVersionTypeId={str(self.LoaderNmbr)}"
-        
+
         self.Options = {"Controlify (1.19.4 -> latest)": self.Controlify,
                    "MidnightControls (1.18 -> latest)": self.MidnightControls,
                    "Controllable for Forge (1.12.2 -> 1.20.1)": self.Controllable_Fo,
                    "Controllable for Fabric (1.19.4 , 1.20.1)": self.Controllable_Fa,
-                   "Framework API for Controllables": self.Framework}
+                   "Framework API for Controllables": self.Framework,
+                   "MidnightLib API for MidnightControls": self.MidnightLib}
 
     def Window(self):
         """
@@ -45,14 +47,21 @@ class CFimpl :
 
         root = ctk.CTk()
         root.title("Implementer une manette")
-        root.geometry("400x300")
+        root.geometry("600x380")
 
-        instructionsLabel = ctk.CTkLabel(root, text="Clique sur le mod adapté, telecharge une version, puis glisse le fichier téléchargé dans le dossier 'mods' de l'instance souhaité.",
+        instructionsLabel = ctk.CTkLabel(root, text="Clique sur le(s) mod(s) adapté(s), telecharge une version, puis glisse le fichier téléchargé dans le dossier 'mods' de l'instance souhaité.",
         wraplength=390)
         instructionsLabel.pack(pady=6, padx=5)
 
         for k, i in self.Options.items():
-            ctk.CTkButton(root, text=k, command=lambda url=i: webbrowser.open(url)).pack(fill="x", pady=5, padx=5)
+            if k == "Framework API for Controllables" or k == "MidnightLib API for MidnightControls":
+                if k == "Framework API for Controllables":
+                    APIlabel = ctk.CTkLabel(root, text="API", font=(ctk.CTkFont(), 15)).pack(fill="x", pady=2, padx=5)
+                    ctk.CTkButton(root, text=k, command=lambda url=i: webbrowser.open(url), fg_color="#4DA1A9", hover_color="#2E5077").pack(fill="x", pady=5, padx=5)
+                else:
+                    ctk.CTkButton(root, text=k, command=lambda url=i: webbrowser.open(url), fg_color="#4DA1A9", hover_color="#2E5077").pack(fill="x", pady=5, padx=5)
+            else:
+                ctk.CTkButton(root, text=k, command=lambda url=i: webbrowser.open(url)).pack(fill="x", pady=5, padx=5)
         
         finiButton = ctk.CTkButton(root, text="Fini !", command=lambda:root.destroy())
         finiButton.pack(pady=10, padx=5)
